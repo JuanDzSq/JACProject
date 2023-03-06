63af906f0d3f00610af0fb21dcbe90283b2f0036
Juan Diaz <juan.dz@techexchange.in>
2023-03-06 04:03:52 +0000

    Completed sign up, login, and logout requirements

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,5 +1,4 @@
-from flask import render_template
-
+from flask import Flask, render_template, request, redirect, url_for, session
 
 def make_endpoints(app):
 
@@ -7,8 +6,40 @@ def make_endpoints(app):
     # go to a specific route on the project's website.
     @app.route("/")
     def home():
-        # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
-        # to render main.html on the home page.
         return render_template("main.html")
 
-    # TODO(Project 1): Implement additional routes according to the project requirements.
+    @app.route("/sign_up", methods =['GET', 'POST'])
+    def sign_up():
+        message = ''
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            account_check = sign_up(username, password)
+            if account_check:
+                session['loggedin'] = True
+                session['username'] = username
+                return render_template('main.html')
+            else:
+                message = 'This username is already in use.'
+        return render_template('sign_up.html', message = message)
+
+    @app.route("/login", methods =['GET', 'POST'])
+    def login():
+        message = ''
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            account_check = sign_in(username, password)
+            if account_check:
+                session['loggedin'] = True
+                session['username'] = username
+                return render_template('main.html')
+            else:
+                message = 'Incorrect username or password.]'
+        return render_template('login.html', message = message)
+
+    @app.route("/logout", methods =['GET', 'POST'])
+    def logout():
+        session.pop('loggedin', None)
+        session.pop('username', None)
+        redirect(url_for('home'))
