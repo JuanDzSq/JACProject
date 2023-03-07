e4d9082eafd7ff0f48911713785f5594e0b3e549
Juan Diaz <juan.dz@techexchange.in>
2023-03-07 04:14:59 +0000

    Completed upload requirement

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,3 +1,4 @@
+from flaskr import backend
 from flask import Flask, render_template, request, redirect, url_for, session
 
 def make_endpoints(app):
@@ -14,14 +15,15 @@ def make_endpoints(app):
         if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
             username = request.form['username']
             password = request.form['password']
-            account_check = sign_up(username, password)
+            account_check = Backend.sign_up(username, password)
             if account_check:
                 session['loggedin'] = True
                 session['username'] = username
                 return render_template('main.html')
             else:
                 message = 'This username is already in use.'
-        return render_template('sign_up.html', message = message)
+
+        return render_template('sign_up.html', message=message)
 
     @app.route("/login", methods =['GET', 'POST'])
     def login():
@@ -29,17 +31,38 @@ def make_endpoints(app):
         if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
             username = request.form['username']
             password = request.form['password']
-            account_check = sign_in(username, password)
+            account_check = Backend.sign_in(username, password)
             if account_check:
                 session['loggedin'] = True
                 session['username'] = username
                 return render_template('main.html')
             else:
                 message = 'Incorrect username or password.]'
-        return render_template('login.html', message = message)
+
+        return render_template('login.html', message=message)
 
     @app.route("/logout", methods =['GET', 'POST'])
     def logout():
         session.pop('loggedin', None)
         session.pop('username', None)
         redirect(url_for('home'))
+
+    @app.route("/upload", methods =['GET', 'POST'])
+    def upload():
+        message = ''
+        allowed_types = set(['md', 'png', 'jpg', 'jpeg'])
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                message = 'File did not input'
+                return render_template('upload.html', message=message)
+
+            f = request.files['file']
+            if f.filename == '':
+                message = 'File not chosen'
+            elif f and '.' in f.filename and f.filename.rsplit('.', 1)[1].lower() in allowed_types:
+                message = 'Upload succesfull'
+                Backend.upload(f, f.filename)
+            else:
+                message = 'Only submit file types that are md, png, jpg, jpeg'
+
+        return render_template('upload.html', message=message)
