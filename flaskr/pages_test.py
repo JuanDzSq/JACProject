92ac60bbec12976c05a1a412f571a7324386a3ef
Juan Diaz <juan.dz@techexchange.in>
2023-04-04 15:27:52 +0000

    commented non-working unit tests

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -63,24 +63,21 @@ def test_sign_up_failed(client):
 """
 Testing if the page will be accessed
 """
-def test_working_get_pages():
-    test_page = {'name_page': 'Survival.html'}
-    assert b"<title>Survival<title>" in test_page
+# def test_working_get_pages():
+#     test_page = {'name_page': 'Survival.html'}
+#     assert b"<title>Survival<title>" in test_page
     
 """
 Testing if the navigation bar will access a link
 """
-def test_working_nav_bar(self):
-    response = self.nav_bar.get('/', follow_redirects=True )
-    self.assertEqual(response.status_code)
+# def test_working_nav_bar(self):
+#     response = self.nav_bar.get('/', follow_redirects=True )
+#     self.assertEqual(response.status_code)
 
 """
 Testing if authors images show
 """
-def test_working_about(self):
-    author = {'Christin': 'Christin.jpeg'}
-    response = self.post("/about", data = author)
-    self.assertEqual(response.status_code)
-
-
-    
+# def test_working_about(self):
+#     author = {'Christin': 'Christin.jpeg'}
+#     response = self.post("/about", data = author)
+#     self.assertEqual(response.status_code)
